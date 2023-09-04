f1c7e118ff3d32535605dcb812ab31eec31d9874
Samridh-Mahajan <133039483+Samridh-Mahajan@users.noreply.github.com>
2023-09-05 02:13:06 +0530

    Add files via upload

---
+++ src/Components/Bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Bar.css';
+
+function CenteredBar() {
+  return (
+    <div className="Centered">
+      <p>New Post</p>
+    
+    </div>
+  );
+}
+
+export default CenteredBar;
---
+++ src/Components/OptionsSelector.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './OptionsSelector.css';
+import CustomSection from './CustomSection';
+import ArticleSection from './Article';
+
+
+const OptionsSelector = () => {
+  const [selectedOption, setSelectedOption] = useState('optionA');
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+ 
+  };
+
+  return (
+    <div className='SelectOption'>
+      <p>Select an option: 
+        <span className='Options'> 
+          <span
+            className={`Option  ${selectedOption === 'optionA' ? 'selected' : ''}`}
+            onClick={() => handleOptionClick('optionA')}
+          >
+            {selectedOption === 'optionA' && <span className='SelectedIndicator'>&#9679;</span>}
+            Question
+          </span>
+          <span
+            className={`Option ${selectedOption === 'optionB' ? 'selected' : ''}`}
+            onClick={() => handleOptionClick('optionB')}
+          > 
+            {selectedOption === 'optionB' && <span className='SelectedIndicator'>&#9679;</span>}
+            Article
+          </span>
+        </span> 
+      </p>
+
+      
+      {selectedOption === 'optionA' ? (
+        <CustomSection />
+        ) : (
+          <ArticleSection />
+      )}
+    </div>
+  );
+};
+
+export default OptionsSelector;
